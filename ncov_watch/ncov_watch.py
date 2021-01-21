2f162624386596ccf56664d8a524dd47c467edd9
usajusaj <usajusaj@usajusaj.org>
2021-01-20 23:49:44 -0500

    Make argparse more user friendly, small syntax fixes

--- ncov_watch/ncov_watch.py
+++ ncov_watch/ncov_watch.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
-import argparse
 import pysam
 import sys
 import csv
 import os
 import pkg_resources
 from pathlib import Path
+
 
 class Variant:
     def __init__(self, contig, position, reference, alt):
@@ -15,17 +15,18 @@ class Variant:
         self.reference = reference
         self.alt = alt
         self.name = None
+
     def key(self):
         return ",".join([str(x) for x in [self.contig, self.position, self.reference, self.alt]])
 
+
 def load_vcf(filename):
     variants = list()
-    f = pysam.VariantFile(filename,'r')
+    f = pysam.VariantFile(filename, 'r')
     for record in f:
         if len(record.alts) > 1:
             sys.stderr.write("Multi-allelic VCF not supported\n")
             sys.exit(1)
-
 
         v = Variant(record.chrom, record.pos, record.ref, record.alts[0])
         if "Name" in record.info:
@@ -34,6 +35,8 @@ def load_vcf(filename):
 
     return variants
 
+
+# noinspection PyBroadException
 def load_ivar_variants(filename):
     variants = list()
     try:
@@ -53,54 +56,66 @@ def load_ivar_variants(filename):
         pass
     return variants
 
-def get_from_directory():
-    find_files = lambda pattern : [ path for path in Path(args.directory).rglob(pattern) ]
-    files = find_files("*pass.vcf") + find_files("*pass.vcf.gz") + find_files("*variants.tsv")
-    for f in files:
-        yield str(f)
+
+def get_from_directory(directory):
+    def _glob(pattern):
+        return [path for path in Path(directory).rglob(pattern)]
+
+    def _inner():
+        files = _glob("*pass.vcf") + _glob("*pass.vcf.gz") + _glob("*variants.tsv")
+        for f in files:
+            yield str(f)
+    return _inner
+
 
 def get_from_stdin():
     for line in sys.stdin:
         yield line.rstrip()
 
+
 def main():
-
-    description = 'Report samples containing a variant in the watchlist'
-    parser = argparse.ArgumentParser(description=description)
-
     mutation_sets = pkg_resources.resource_listdir(__name__, 'watchlists')
     mutation_sets = [Path(mutation_set).stem for mutation_set in mutation_sets]
-    parser.add_argument('-m', '--mutation_set', required=True,
+
+    import argparse
+    parser = argparse.ArgumentParser(description='Report samples containing a variant in the watchlist')
+    parser.add_argument('-m', '--mutation-set', required=True,
                         choices=mutation_sets,
                         help='Mutation set to screen variants against')
+    parser.add_argument('-o', '--output',
+                        type=argparse.FileType('w'),
+                        default=sys.stdout,
+                        help='Write output to file, defaults to stdout')
+    parser.add_argument('input', nargs='?',
+                        help='Root of directories holding variant files, blank for stdin')
 
-    parser.add_argument('-d', '--directory', help='root of directories holding variant files')
     args = parser.parse_args()
+    out = csv.writer(args.output, delimiter='\t')
 
     mutation_set_path = Path("watchlists") / Path(args.mutation_set + ".vcf")
     mutation_set = pkg_resources.resource_filename(__name__, str(mutation_set_path))
 
     watch_variants = load_vcf(mutation_set)
-    watch_dict = dict()
-    for v in watch_variants:
-        watch_dict[v.key()] = v.name
+    watch_dict = {v.key(): v.name for v in watch_variants}
 
     gen_func = get_from_stdin
-    if args.directory:
-        gen_func = get_from_directory
+    if args.input:
+        gen_func = get_from_directory(args.input)
 
-    print("\t".join(["sample", "mutation", "contig", "position", "reference", "alt"]))
+    out.writerow(["sample", "mutation", "contig", "position", "reference", "alt"])
     for f in gen_func():
         if f.find("variants.tsv") >= 0:
             variants = load_ivar_variants(f)
         else:
             variants = load_vcf(f)
+
         for v in variants:
             if v.key() in watch_dict:
                 name = watch_dict[v.key()]
                 if name is None:
                     name = "not annotated"
-                print("\t".join([os.path.basename(f), name, v.contig, str(v.position), v.reference, v.alt]))
+                out.writerow([os.path.basename(f), name, v.contig, str(v.position), v.reference, v.alt])
+
 
 if __name__ == "__main__":
     main()
